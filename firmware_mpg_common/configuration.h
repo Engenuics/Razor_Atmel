321ff4081e75030781c57bb6b70138c93f59ad54
Engenuics Support <support@engenuics.com>
2016-03-04 20:35:53 -0700

    Updated project files

--- firmware_mpg_common/configuration.h
+++ firmware_mpg_common/configuration.h
@@ -22,15 +22,12 @@ ANT BOARDTEST CHANNEL CONFIG: 4660 (0x1234), 96(0x60), 1
 /**********************************************************************************************************************
 Runtime switches
 ***********************************************************************************************************************/
-#define MPGL1             1         /* Use to activate MPG Level 1 specific code */
-//#define MPGL2             1         /* Use to activate MPG Level 2 (all versions) specific code */
-//#define MPGL2_R01          1         /* Use with MPGL2-EHDW-01 revision board */
+//#define MPGL2_R01                   /* Use with MPGL2-EHDW-01 revision board */
 
-#define SIMULATOR_MODE    1         /* Define to enable code to run in IAR simulator */
-#define DEBUG_MODE        1         /* Define to enable certain debugging code */
-#define STARTUP_SOUND     1         /* Define to include buzzer sound on startup */
+#define DEBUG_MODE                /* Define to enable certain debugging code */
+//#define STARTUP_SOUND              /* Define to include buzzer sound on startup */
 
-//#define USE_SIMPLE_USART0 1  /* Define to use USART0 as a very simple byte-wise UART for debug purposes */
+//#define USE_SIMPLE_USART0   /* Define to use USART0 as a very simple byte-wise UART for debug purposes */
 
 /**********************************************************************************************************************
 Type Definitions
@@ -264,7 +261,7 @@ The order of the definitions below must match the order of the definitions provi
 /*----------------------------------------------------------------------------------------------------------------------
 %BUZZER% Buzzer Configuration                                                                                                  
 ------------------------------------------------------------------------------------------------------------------------
-MPGL1 has two buzzers, MPGL2 only has one */
+MPG1 has two buzzers, MPG2 only has one */
 
 #define BUZZER1               AT91C_PWMC_CHID0
 
