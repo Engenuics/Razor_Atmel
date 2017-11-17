8aaac5392a87ac088dfaacb4f3ef77c58bac614f
Engenuics <jason.long@engenuics.com>
2017-11-17 23:38:34 +0800

    Setup for FW2 module

--- firmware_common/drivers/adc12.c
+++ firmware_common/drivers/adc12.c
@@ -11,37 +11,14 @@ channel on the ADC at a time.  Any averaging or special operations should be han
 application using the driver.  This driver is set up as a state machine for future
 feature additions.
 
-The first sample tends to read 20-30 bits high.  If no sample is taken for a few minutes,
-the next first sample will also read high.  This implies a long time constant in the hold
-time, but the timing parameters that have been set all line up with the electrical
-characteristics and source impedence considerations.  So this is a mystery for
-now -- suggest the first sample is thrown out, or average it out with at least 16 samples
-per displayed result which will reduce the error down to 1 or 2 LSBs.  
+
 
 ------------------------------------------------------------------------------------------------------------------------
 API:
 
 TYPES
-Adc12ChannelType {ADC12_CH0...ADC12_CH7}
 
 PUBLIC FUNCTIONS
-void Adc12AssignCallback(Adc12ChannelType eAdcChannel_, fnCode_u16_type fpUserCallback_)
-Assigns callback for the client application.  This is how the ADC result for any channel
-is accessed.  The callback function must have one u16 parameter where the result is passed.
-Different callbacks may be assigned for each channel. 
-
-e.g. to read AN0 from the blade connector (which is channel 2 on the ADC):
-void UserApp_AdcCallback(u16 u16Result_);
-...
-Adc12AssignCallback(ADC12_CH2, UserApp_AdcCallback);
-
-
-bool Adc12StartConversion(Adc12ChannelType eAdcChannel_)
-Checks if the ADC is available and starts the conversion on the selected channel.
-Returns TRUE if the conversion is started; returns FALSE if the ADC is not available.
-e.g.
-bool bConversionStarted = FALSE;
-bConversionStarted = Adc12StartConversion(ADC12_CH2);
 
 
 PROTECTED FUNCTIONS
@@ -94,89 +71,6 @@ Function Definitions
 /* Public functions                                                                                                   */
 /*--------------------------------------------------------------------------------------------------------------------*/
 
-/*----------------------------------------------------------------------------------------------------------------------
-Function: Adc12AssignCallback
-
-Description
-Allows user to specify a custom callback function for when the ADC12 interrupt occurs.
-
-Requires:
-  - eAdcChannel_ is the channel to which the callback will be assigned
-  - fpUserCallback_ is the function address (name) for the user's callback
-
-Promises:
-  - Adc12_fpCallbackCh<eAdcChannel_> loaded with fpUserCallback_
-*/
-void Adc12AssignCallback(Adc12ChannelType eAdcChannel_, fnCode_u16_type fpUserCallback_)
-{
-  bool bChannelValid = FALSE;
-
-  /* Check to ensure the requested channel exists */
-  for(u8 i = 0; i < (sizeof(Adc12_aeChannels) / sizeof (Adc12ChannelType)); i++)
-  {
-    if(Adc12_aeChannels[i] == eAdcChannel_)
-    {
-      bChannelValid = TRUE;
-    }
-  }
-  
-  /* If the channel is valid, then assign the new callback function */
-  if(bChannelValid)
-  {
-    Adc12_afCallbacks[eAdcChannel_] = fpUserCallback_;
-  }
-  else
-  {
-    DebugPrintf("Invalid channel\n\r");
-  }
-  
-} /* end Adc12AssignCallback() */
-
-
-/*----------------------------------------------------------------------------------------------------------------------
-Function: Adc12StartConversion
-
-Description
-Starts the conversion on the selected channel if the ADC is ready.
-If the ADC is busy, the function returns FALSE so the calling application knows
-to wait and try later.
-
-Requires:
-  - eAdcChannel_ is the ADC12 channel to disable
-  - Adc12_bAdcAvailable indicates if the ADC is available for a conversion
-
-Promises:
-If Adc12_bAdcAvailable is TRUE:
-  - Adc12_bAdcAvailable changed to false
-  - ADC12B_CHER bit for eAdcChannel_ is set
-  - ADC12B_IER bit for eAdcChannel_is set
-  - Returns TRUE
-
-If Adc12_bAdcAvailable is FALSE:
-  - Returns FALSE
-*/
-bool Adc12StartConversion(Adc12ChannelType eAdcChannel_)
-{
-  if(Adc12_bAdcAvailable)
-  {
-    /* Take the semaphore so we have the ADC resource.  Since this is a binary semaphore 
-    that is only cleared in the ISR, it is safe to do this with interrupts enabled */
-    Adc12_bAdcAvailable = FALSE;
-   
-    /* Enable the channel and its interrupt */
-    AT91C_BASE_ADC12B->ADC12B_CHER = (1 << eAdcChannel_);
-    AT91C_BASE_ADC12B->ADC12B_IER  = (1 << eAdcChannel_);
-  
-    /* Start the conversion and exit */
-    AT91C_BASE_ADC12B->ADC12B_CR |= AT91C_ADC12B_CR_START;
-    return TRUE;
-  }
-  
-  /* The ADC is not available */
-  return FALSE;
-
-} /* end Adc12StartConversion() */
-
 
 /*--------------------------------------------------------------------------------------------------------------------*/
 /* Protected functions                                                                                                */
@@ -196,34 +90,10 @@ Promises:
 */
 void Adc12Initialize(void)
 {
-  u8 au8Adc12Started[] = "ADC12 task initialized\n\r";
-
-  /* Initialize peripheral registers. ADC starts totally disabled. */
-  AT91C_BASE_ADC12B->ADC12B_MR   = ADC12B_MR_INIT;
-  AT91C_BASE_ADC12B->ADC12B_CHDR = ADC12B_CHDR_INIT;
-  AT91C_BASE_ADC12B->ADC12B_ACR  = ADC12B_ACR_INIT;
-  AT91C_BASE_ADC12B->ADC12B_EMR  = ADC12B_EMR_INIT;
-  AT91C_BASE_ADC12B->ADC12B_IDR  = ADC12B_IDR_INIT;
-  
-  /* Set all the callbacks to default */
-  for(u8 i = 0; i < (sizeof(Adc12_afCallbacks) / sizeof(fnCode_u16_type)); i++)
-  {
-    Adc12_afCallbacks[i] = Adc12DefaultCallback;
-  }
-  
-  /* Mark the ADC semaphore as available */
-  Adc12_bAdcAvailable = TRUE;
   
   /* Check initialization and set first state */
   if( 1 )
   {
-    /* Enable required interrupts */
-    NVIC_ClearPendingIRQ(IRQn_ADCC0);
-    NVIC_EnableIRQ(IRQn_ADCC0);
-    
-    /* Write message, set "good" flag and select Idle state */
-    DebugPrintf(au8Adc12Started);
-    G_u32ApplicationFlags |= _APPLICATION_FLAGS_ADC;
     Adc12_StateMachine = Adc12SM_Idle;
   }
   else
@@ -259,68 +129,6 @@ void Adc12RunActiveState(void)
 /*--------------------------------------------------------------------------------------------------------------------*/
 /* Private functions                                                                                                  */
 /*--------------------------------------------------------------------------------------------------------------------*/
-
-/*----------------------------------------------------------------------------------------------------------------------
-Function Adc12DefaultCallback()
-
-Description:
-An empty function that the unset Adc Callbacks point to.  Expected that the 
-user will set their own.
-
-Requires:
-  - 
-
-Promises:
-  - 
-*/
-void Adc12DefaultCallback(u16 u16Result_)
-{
-} /* End Adc12DefaultCallback() */
-
-
-/*----------------------------------------------------------------------------------------------------------------------
-ISR: ADCC0_IrqHandler
-
-Description:
-Parses the ADC12 interrupts and handles them appropriately.  Note that all ADC12
-interrupts are ORed and will trigger this handler, therefore any expected interrupt 
-that is enabled must be parsed out and handled.  There is no obviously available
-explanation for why this handler is called ADCC0_IrqHandler instead of ADC12B_IrqHandler
-
-Requires:
-  - Only one channel can be converting at a time, so only one interrupt flag
-    will be set.
-
-Promises:
-  - 
-*/
-void ADCC0_IrqHandler(void)
-{
-  u16 u16Adc12Result;
-  /* WARNING: if you step through this handler with the ADC12B registers
-  debugging, the debugger reads ADC12B_SR and clears the EOC flag bits */
-  
-
-  /* Check through all the available channels */
-  for(u8 i = 0; i < (sizeof(Adc12_aeChannels) / sizeof(Adc12ChannelType)); i++)
-  {
-    if(AT91C_BASE_ADC12B->ADC12B_SR & (1 << Adc12_aeChannels[i]))
-    {
-      /* Read the channel's result register (clears EOC bit / interrupt) and send to callback */
-      u16Adc12Result = AT91C_BASE_ADC12B->ADC12B_CDR[Adc12_aeChannels[i]];
-      Adc12_afCallbacks[Adc12_aeChannels[i]](u16Adc12Result);
-      
-      /* Disable the channel and exit the loop since only one channel can be set */
-      AT91C_BASE_ADC12B->ADC12B_CHDR = (1 << Adc12_aeChannels[i]);
-      break;
-    }
-  }
-  
-  /* Give the Semaphore back, clear the ADC pending flag and exit */
-  Adc12_bAdcAvailable = TRUE;
-  NVIC->ICPR[0] = (1 << AT91C_ID_ADC12B);
-  
-} /* end ADCC0_IrqHandler() */
 
 
 /**********************************************************************************************************************
--- firmware_common/drivers/adc12.h
+++ firmware_common/drivers/adc12.h
@@ -12,49 +12,47 @@ Header file for adc12.c
 /**********************************************************************************************************************
 Type Definitions
 **********************************************************************************************************************/
-typedef enum {ADC12_CH0 = 0, ADC12_CH1 = 1, ADC12_CH2 = 2, ADC12_CH3 = 3, 
-              ADC12_CH4 = 4, ADC12_CH5 = 5, ADC12_CH6 = 6, ADC12_CH7 = 7} Adc12ChannelType;
 
 
 /**********************************************************************************************************************
 Constants / Definitions
 **********************************************************************************************************************/
-#define ADC12B_MR_INIT (u32)0x0F041700
+#define ADC12B_MR_INIT (u32)0x
 /*
     31 [0] Reserved
     30 [0] "
     29 [0] "
     28 [0] "
 
-    27 [1] SHTIM set for maximum time to allow for maximum source impedance
-    26 [1] "
-    25 [1] "
-    24 [1] "
+    27 [?] SHTIM set for maximum time to allow for maximum source impedance
+    26 [?] "
+    25 [?] "
+    24 [?] "
 
-    23 [0] STARTUP set for maximum startup time at 1MHz ADC clock
-    22 [0] "
-    21 [0] "
-    20 [0] "
+    23 [?] STARTUP set for maximum startup time at 1MHz ADC clock
+    22 [?] "
+    21 [?] "
+    20 [?] "
 
-    19 [0] "
-    18 [1] "
-    17 [0] "
-    16 [0] "
+    19 [?] "
+    18 [?] "
+    17 [?] "
+    16 [?] "
 
-    15 [0] PRESCAL is 23 (gives 1MHz ADC clock)
-    14 [0] "
-    13 [0] "
-    12 [1] "
+    15 [?] PRESCAL is xx? (gives 1MHz ADC clock)
+    14 [?] "
+    13 [?] "
+    12 [?] "
 
-    11 [0] "
-    10 [1] "
-    09 [1] "
-    08 [1] "
+    11 [?] "
+    10 [?] "
+    09 [?] "
+    08 [?] "
 
     07 [0] Reserved
     06 [0] "
-    05 [0] SLEEP Normal mode
-    04 [0] LOWRES 12-bit resolution
+    05 [?] SLEEP Normal mode
+    04 [?] LOWRES 12-bit resolution
 
     03 [0] TRGSEL not applicable
     02 [0] "
@@ -62,22 +60,22 @@ Constants / Definitions
     00 [0] TRGEN Hardware triggers disabled
 */
 
-#define ADC12B_CHDR_INIT (u32)0x000000FF
+#define ADC12B_CHDR_INIT (u32)0x
 /*
     31-08 [0] Reserved
 
-    07 [1] CH7 disabled
-    06 [1] CH6 disabled
-    05 [1] CH5 disabled
-    04 [1] CH4 disabled
+    07 [?] CH7 
+    06 [?] CH6 
+    05 [?] CH5 
+    04 [?] CH4 
 
-    03 [1] CH3 disabled
-    02 [1] CH2 disabled
-    01 [1] CH1 disabled
-    00 [1] CH0 disabled
+    03 [?] CH3 
+    02 [?] CH2 
+    01 [?] CH1 
+    00 [?] CH0 
 */
 
-#define ADC12B_ACR_INIT (u32)0x00000101
+#define ADC12B_ACR_INIT (u32)0x
 /*
     31 [0] Reserved 
     30 [0] "
@@ -96,8 +94,8 @@ Constants / Definitions
 
     19 [0] "
     18 [0] "
-    17 [0] OFFSET Vrefin/2G
-    16 [0] DIFF Single ended mode
+    17 [?] OFFSET 
+    16 [?] DIFF 
 
     15 [0] Reserved
     14 [0] "
@@ -106,8 +104,8 @@ Constants / Definitions
 
     11 [0] "
     10 [0] "
-    09 [0] IBCTL Bias current control typical
-    08 [1] 
+    09 [?] IBCTL Bias current control typical
+    08 [?] 
 
     07 [0] Reserved
     06 [0] "
@@ -116,62 +114,33 @@ Constants / Definitions
 
     03 [0] "
     02 [0] "
-    01 [0] GAIN 1
-    00 [1] "
+    01 [?] GAIN 1
+    00 [?] "
 */
 
 
-#define ADC12B_EMR_INIT (u32)0x00040001
+#define ADC12B_EMR_INIT (u32)0x
 /*
     31-24 [0] Reserved
 
-    23 [0] OFF_MODE_STARTUP_TIME target 40us
-    22 [0] (4 + 1)*8/1MHz = 40us
-    21 [0] "
-    20 [0] "
+    23 [?] OFF_MODE_STARTUP_TIME 
+    22 [?] "
+    21 [?] "
+    20 [?] "
 
-    19 [0] "
-    18 [1] "
-    17 [0] "
-    16 [0] "
+    19 [?] "
+    18 [?] "
+    17 [?] "
+    16 [?] "
 
     15-4 [0] Reserved
 
     03 [0] Reserved
     02 [0] "
     01 [0] "
-    00 [1] OFFMODES Use off mode
+    00 [?] OFFMODES Use off mode
 */
 
-#define ADC12B_IDR_INIT (u32)0x000FFFFF
-/*
-    31 - 20 [0] Reserved
-
-    19 [1] RXBUFF: Receive Buffer Full Interrupt Disabled
-    18 [1] ENDRX: End of Receive Buffer Interrupt Disabled
-    17 [1] GOVRE: General Overrun Error Interrupt Disabled
-    16 [1] DRDY: Data Ready Interrupt Disabled
-
-    15 [1] OVRE7: Overrun Error Interrupt Disabled
-    14 [1] OVRE6: Overrun Error Interrupt Disabled
-    13 [1] OVRE5: Overrun Error Interrupt Disabled
-    12 [1] OVRE4: Overrun Error Interrupt Disabled
-
-    11 [1] OVRE3: Overrun Error Interrupt Disabled
-    10 [1] OVRE2: Overrun Error Interrupt Disabled
-    09 [1] OVRE1: Overrun Error Interrupt Disabled
-    08 [1] OVRE0: Overrun Error Interrupt Disabled
-
-    07 [1] EOC7: End of Conversion Interrupt Disabled
-    06 [1] EOC6: End of Conversion Interrupt Disabled
-    05 [1] EOC5: End of Conversion Interrupt Disabled
-    04 [1] EOC4: End of Conversion Interrupt Disabled
-
-    03 [1] EOC3: End of Conversion Interrupt Disabled
-    02 [1] EOC2: End of Conversion Interrupt Disabled
-    01 [1] EOC1: End of Conversion Interrupt Disabled
-    00 [1] EOC0: End of Conversion Interrupt Disabled
-*/
 
 /**********************************************************************************************************************
 Function Declarations
@@ -180,13 +149,6 @@ Function Declarations
 /*--------------------------------------------------------------------------------------------------------------------*/
 /* Public functions                                                                                                   */
 /*--------------------------------------------------------------------------------------------------------------------*/
-#if 0
-void Adc12EnableChannel(Adc12ChannelType eAdcChannel_);
-void Adc12DisableChannel(Adc12ChannelType eAdcChannel_);
-#endif
-
-bool Adc12StartConversion(Adc12ChannelType eAdcChannel_);
-void Adc12AssignCallback(Adc12ChannelType eAdcChannel_, fnCode_u16_type fpUserCallback_);
 
 
 /*--------------------------------------------------------------------------------------------------------------------*/
@@ -199,7 +161,6 @@ void Adc12RunActiveState(void);
 /*--------------------------------------------------------------------------------------------------------------------*/
 /* Private functions                                                                                                  */
 /*--------------------------------------------------------------------------------------------------------------------*/
-void Adc12DefaultCallback(u16 u16Result_);
 
 
 /***********************************************************************************************************************
